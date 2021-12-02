54873dddfb8b7d6bfec4ca47ce127501b2775b0b
CarlosHernandezL <34498991+CarlosHernandezL@users.noreply.github.com>
2021-12-01 19:16:57 -0600

    Create application.py

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def index():
+    return "Your Flask App Works from TC!"
+
+@application.route("/hello")
+def hello():
+    return "Hello World TC!"
+
+
+if __name__ == "__main__":
+    application.run(port=5000, debug=True)
